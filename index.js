8f0f82dbd08d11687c128e885fc1754f9de78c35
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-24 19:56:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const {fetchISSFlyOverTimes} = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -11,6 +12,7 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
+/*
 fetchCoordsByIP('72.141.213.93', (error, geoCoordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -18,5 +20,16 @@ fetchCoordsByIP('72.141.213.93', (error, geoCoordinates) => {
   }
   console.log('Coordinates are your IP are:' ,  geoCoordinates);
 });
+*/
+/*
+fetchISSFlyOverTimes({ latitude: '45.26910', longitude: '-75.75180' }
+, (error, passes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('Flyover times of ISS for given coordinates are:' ,  passes);
+});
+*/
 
 
