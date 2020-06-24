39f88b61566c61fc05a2ed4c38e9457bca5c359c
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-24 17:51:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+// Write code in the index.js file which will require and call this function, so that it can be visually tested.
+
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
