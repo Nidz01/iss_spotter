9ce2dfdb94b91bab1a775cbf4d7e911162f5bdc0
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-24 18:19:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-
 // Write code in the index.js file which will require and call this function, so that it can be visually tested.
 
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
 });
+
+const ip = '72.141.213.93';
+
+fetchCoordsByIP((ip, data) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Coordinates by IP are:' , ip);
+});
+
+
