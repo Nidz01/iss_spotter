704b7ba7173ea2081a5efd3edb254a7a7697aa82
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-24 21:35:47 -0400

    Update index.js
    5 pass times for the ISS.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
 const {fetchISSFlyOverTimes} = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
+/*
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -11,7 +13,7 @@ fetchMyIP((error, ip) => {
   }
   console.log('It worked! Returned IP:' , ip);
 });
-
+*/
 /*
 fetchCoordsByIP('72.141.213.93', (error, geoCoordinates) => {
   if (error) {
@@ -31,5 +33,18 @@ fetchISSFlyOverTimes({ latitude: '45.26910', longitude: '-75.75180' }
   console.log('Flyover times of ISS for given coordinates are:' ,  passes);
 });
 */
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+});
 
 
